f113d7075e761c88b8845d2784cfaad8cd5668df
mahfuz-7148 <mrahman7148@gmail.com>
2025-03-13 04:13:40 +0600

    updated

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const answerOptions = document.querySelector(".answerOptions");
+const nextQuestionBtn = document.querySelector(".nextQuestionBtn");
 
 let quizCategory = "programming";
+let currentQuestion = null
 
 const getRandomQuestion = () => {
   const categoryQuestions = questions.find(
@@ -11,22 +13,37 @@ const getRandomQuestion = () => {
     return randomQuestion;
 };
 
-const renderQuestions = () => {
-    const currentQuestion = getRandomQuestion()
-    if (!currentQuestion) return
+
+
+const handleAnswer = (option, answerIndex) => {
+  const isCorrect = currentQuestion.correctAnswer === answerIndex;
+  option.classList.add(isCorrect ? 'correct' : 'incorrect')
+
+  !isCorrect ? hightlightCorrect() : ''
+
+  answerOptions.querySelectorAll(".answerOption").forEach(option => option.style.pointerEvents = 'none')
+}
+
+const renderQuestion = () => {
+    currentQuestion = getRandomQuestion()
+  if (!currentQuestion) return
+  console.log(currentQuestion)
 
     answerOptions.innerHTML = ''
     document.getElementById("questionText").textContent = currentQuestion.question
 
 
-    currentQuestion.options.forEach(option => {
+    currentQuestion.options.forEach((option, index) => {
         const li = document.createElement('li')
         li.className =
-          "flex items-center p-2 bg-purple-100 rounded text-gray-900 font-medium";
+          "flex items-center p-2 bg-purple-100 rounded text-gray-900 font-medium cursor-pointer transition-colors duration-300 ease-in-out hover:bg-[#dad5fd]";
         li.classList.add("answerOption");
         li.textContent = option
-        answerOptions.appendChild(li)
+      answerOptions.appendChild(li)
+      li.addEventListener("click", () => handleAnswer(li, index));
     });
 }
 
-renderQuestions()
+renderQuestion()
+
+nextQuestionBtn.addEventListener('click', renderQuestion)
